1c5a1999b3348029c444fc1a55c9d73de5348bc2
Andrew Forward <aforward@gmail.com>
2017-01-30 13:20:50 -0500

    Add some DocTest examples

--- lib/sampleplug/message.ex
+++ lib/sampleplug/message.ex
@@ -1,5 +1,18 @@
 defmodule Sampleplug.Message do
   
+
+  @doc ~S"""
+  A welcome message, you can provide an optional name otherwise
+  get a generic Hello World.
+
+  ## Examples
+
+      iex> Sampleplug.Message.hello
+      "Hello World"
+
+      iex> Sampleplug.Message.hello("James")
+      "Hello James"
+  """
   def hello, do: hello(nil)
   def hello(nil), do: "Hello World"
   def hello(name), do: "Hello #{name}"
