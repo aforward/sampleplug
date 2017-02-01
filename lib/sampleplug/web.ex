db04cca6478aa30362051c3b61b72f9aff8f01dc
Andrew Forward <aforward@gmail.com>
2017-02-01 12:25:00 -0500

    Add a post route

--- lib/sampleplug/web.ex
+++ lib/sampleplug/web.ex
@@ -7,6 +7,7 @@ defmodule Sampleplug.Web do
   plug :match
   plug :dispatch
 
+
   def init(options) do
     options
   end
@@ -21,8 +22,16 @@ defmodule Sampleplug.Web do
   get "/" do
     conn 
     |> Plug.Conn.put_resp_content_type("text/html") 
-    |> Plug.Conn.send_resp(200, layout(&template_homepage/1, %{head_title: "Homepage", name: "World"}))
+    |> Plug.Conn.send_resp(200, layout(&template_homepage/1, %{head_title: "Homepage", h1: "Hello World"}))
   end
+
+  post "/" do
+    conn = parse(conn)
+    conn 
+    |> Plug.Conn.put_resp_content_type("text/html") 
+    |> Plug.Conn.send_resp(200, layout(&template_homepage/1, %{head_title: "Homepage", h1: conn.params["newH1"]}))
+  end
+
 
   get "/hello/:name" do
     view = %{head_title: "Hello #{name}", name: name}
@@ -35,6 +44,14 @@ defmodule Sampleplug.Web do
     conn
     |> send_resp(404, "This isn't the page you are looking for")
   end 
+
+
+  # TODO: make it work with Plug.Parsers
+  # plug Plug.Parsers, parsers: [:urlencoded, :multipart]
+  def parse(conn, opts \\ []) do
+    opts = Keyword.put_new(opts, :parsers, [Plug.Parsers.URLENCODED, Plug.Parsers.MULTIPART])
+    Plug.Parsers.call(conn, Plug.Parsers.init(opts))
+  end  
 
   EEx.function_from_file :def, :layout, "lib/templates/layout.eex", [:templatefn, :view]
   EEx.function_from_file :def, :template_homepage, "lib/templates/homepage.eex", [:view]
